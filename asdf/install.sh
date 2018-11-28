037fcf8bb9ac4822c107da6322da60d4387c5edd
Nicolas Blanchot <nico.blanchot@lexisnexis.co.uk>
2018-11-28 11:29:45 +0000

    Add a script to install asdf

---
+++ asdf/install.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
+
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+
+asdf plugin-add erlang
+asdf plugin-add elixir
+asdf plugin-add nodejs
+asdf plugin-add golang
+asdf plugin-add python
+asdf plugin-add ruby
+
+# Install the nodejs OpenPGP keys to main key ring
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+while true; do
+  read -p "Erlang version to install: " erlangVersion
+  read -p "Elixir version to install: " elixirVersion
+  read -p "Node version to install: " nodejsVersion
+  read -p "Go version to install: " golangVersion
+  read -p "Python2 version to install: " python2Version
+  read -p "Python3 version to install: " python3Version
+done
+
+# Install Erlang
+asdf install erlang ${erlangVersion}
+asdf global erlang ${erlangVersion}
+echo "Installed Erlang ${erlangVersion} and defined as global version."
+
+# Install Elixir
+asdf install elixir ${elixirVersion}
+asdf global elixir ${elixirVersion}
+echo "Installed Elixir ${elixirVersion} and defined as global version."
+
+# Install NodeJs
+asdf install nodejs ${nodejsVersion}
+asdf global nodejs ${nodejsVersion}
+echo "Installed Nodejs ${nodejsVersion} and defined as global version."
+
+# Install Golang
+asdf install golang ${golangVersion}
+asdf global golang ${golangVersion}
+echo "Installed Golang ${golangVersion} and defined as global version."
+
+# Install Python 2
+asdf install python ${python2Version}
+asdf global python ${python2Version}
+echo "Installed Python2 ${pythonVersion} and defined as global version."
+
+# Install Python 3
+asdf install python ${python3Version}
+echo "Installed Python3 ${pythonVersion}."
